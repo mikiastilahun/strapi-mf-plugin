1f45dcc38bbd1001be6b3f5adf8f25469124851b
Patrick Riegler <patrick.riegler@grapple-solutions.com>
2026-02-19 19:16:00 +0100

    add Access-Control-Allow-* to rspack config

--- layout-renderer/rspack.config.js
+++ layout-renderer/rspack.config.js
@@ -35,6 +35,8 @@ export default {
     allowedHosts: "all",
     headers: {
       "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization",
     },
   },
 
